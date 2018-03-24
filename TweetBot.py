45e0a1da475eb585417a1430896c866403d01134
cdcasmor <34719773+cdcasmor@users.noreply.github.com>
2018-03-24 11:27:29 -0700

    Third commit

--- TweetBot.py
+++ TweetBot.py
@@ -8,8 +8,8 @@ import os
 # Twitter API Keys
 consumer_key = os.environ["consumer_key"]
 consumer_secret = os.environ["consumer_secret"]
-access_token = os.environ["consumer_token"]
-access_token_secret = os.environ["consumer_token_secret"]
+access_token = os.environ["access_token"]
+access_token_secret = os.environ["access_token_secret"]
 
 # Quotes to Tweet
 happy_quotes = [
